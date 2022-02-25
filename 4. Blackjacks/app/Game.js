8a288cdb1440412b9b81a590bd89c7510bba54dd
Midnajt <midnajt0@gmail.com>
2022-02-25 18:06:43 +0100

    adding cards to dealers hand and players hand done

--- 4. Blackjacks/app/Game.js
+++ 4. Blackjacks/app/Game.js
@@ -1,5 +1,38 @@
+import { Player } from './Player.js';
 import { Deck } from './Deck.js';
+import { Table } from './Table.js';
 
-const deck = new Deck();
+class Game {
+    constructor({player, table}){
+        this.player = player;
+        this.dealer = new Player('Krupier');
+        this.table = table;
+        this.deck = new Deck();
+        this.deck.shuffle();
+    }
 
-// console.log(deck.shuffle());
+    run(){
+        this.dealCards();
+    }
+
+    dealCards(){
+        for(let n = 0; n<2; n++){
+            let card1 = this.deck.pickOne();
+            this.player.hand.addCard(card1);
+            this.table.showPlayersCard(card1.render());
+            
+            let card2 = this.deck.pickOne();
+            this.dealer.hand.addCard(card2)
+            this.table.showDealersCard(card2.render());
+        }
+    }
+}
+
+const table = new Table(document.getElementById('playersCards'),document.getElementById('dealersCards'));
+const player = new Player('Midi');
+const game = new Game({
+    player,
+    table
+});
+
+game.run();
